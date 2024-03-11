155fe6f2af3f7e6c77a37cdb5af9d2a4cedc8650
Faraz Chand <faraz.chand@hotmail.com>
2024-03-10 20:36:24 -0400

    general update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,28 @@
 import React from "react";
 
-const ContactPage = () => {
-  return <div>ContactPage</div>;
-};
+const ContactPage = () => (
+  <main>
+    <div className="container">
+      <section className="contact-section">
+        <p>
+          Have questions, suggestions, or feedback? Please feel free to contact
+          us using the form below:
+        </p>
+        <form action="/submit-contact" method="post">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" name="email" required />
+
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" rows="4" required></textarea>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
+    </div>
+  </main>
+);
 
 export default ContactPage;
