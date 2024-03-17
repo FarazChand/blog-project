cd3c21429978c3b34b0aa8ce2c2b946f8e683dc4
Faraz Chand <faraz.chand@hotmail.com>
2024-03-17 14:08:04 -0400

    general update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -15,4 +15,6 @@ export const socialLinks = [
     name: "linkedin",
     link: "https://www.linkedin.com/in/faraz-chand-7b35442b9/",
   },
+
+  { name: "email", link: "" },
 ];
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BsTwitter } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 import "./Contact.scss";
 
@@ -10,16 +11,29 @@ const ContactPage = () => (
     <div className="container">
       <div className="main-section contact-section">
         <section>
-          <h2>Contact Me</h2>
+          <h2 className="contact-title">Contact Me</h2>
           <div className="contact-links">
-            <div className="link-wrap">
-              <BsTwitter />
-            </div>
-            <div>
-              <FaLinkedin />
-            </div>
-            <div>
-              <MdOutlineMail />
+            <div className="general-container">
+              <Link className="contact-link">
+                <div>
+                  <BsTwitter />
+                </div>
+                <p>My Twitter</p>
+              </Link>
+
+              <Link className="contact-link">
+                <div>
+                  <FaLinkedin />
+                </div>
+                <p>My LinkedIn</p>
+              </Link>
+
+              <Link className="contact-link">
+                <div>
+                  <MdOutlineMail />
+                </div>
+                <p>My Email</p>
+              </Link>
             </div>
           </div>
         </section>
